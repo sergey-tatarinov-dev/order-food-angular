14cc35f240ec60722b496e925d7ca8e0618ca0d9
kurson2234562 <kurson.aaa@yandex.ua>
2018-05-13 11:27:22 +0300

    Switch components and decor from Materialize to Bootstrap

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {Product} from './product.model';
+import {ProductService} from './product.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  products: Product[];
+
+  constructor(private productService: ProductService) {
+  }
+
+  ngOnInit() {
+    console.log('im here');
+    this.productService.getProducts()
+      .subscribe(
+        data => this.products = data,
+         (error) => console.log(error),
+         () => console.log('suscess')
+      );
+  }
+
+}
